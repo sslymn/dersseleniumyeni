4313ba73925944ba05832c7b509263fcde0669e2
sslymn <slymann89@outlook.com>
2024-03-20 18:10:10 +0300

    03

--- test_Sauce.py
+++ test_Sauce.py
@@ -10,127 +10,107 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 
 
-class test_Saurce:
-    def all_setting(self):
-        driver =webdriver.Chrome()
-        driver.maximize_window()
-        driver.get("https://www.saucedemo.com/")  
-        return driver
-    
+class TestSource:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
 
+    def local(self):
+        self.driver.get("https://www.saucedemo.com/")
+        return self.driver
 
- 
-    #BAŞARISIZ GİRİŞ TESTİ
+   
+
+    # BAŞARISIZ GİRİŞ TESTİ
     def test_invalid_login(self):
-        driver = self.all_setting()
-        WebDriverWait(driver,5).until(EC.visibility_of_all_elements_located((By.ID,"user-name")))    
-        userNameInput = driver.find_element(By.ID,"user-name")
-        passwordInput = driver.find_element(By.ID,"password")
+        driver = self.local()
+        WebDriverWait(driver, 5).until(EC.visibility_of_all_elements_located((By.ID, "user-name")))
+        userNameInput = driver.find_element(By.ID, "user-name")
+        passwordInput = driver.find_element(By.ID, "password")
         sleep(1)
         userNameInput.send_keys("deneme")
         passwordInput.send_keys("1")
-        WebDriverWait(driver,5).until(EC.visibility_of_all_elements_located((By.ID,"login-button")))   
-        loginBtn = driver.find_element(By.ID,"login-button")
+        WebDriverWait(driver, 5).until(EC.visibility_of_all_elements_located((By.ID, "login-button")))
+        loginBtn = driver.find_element(By.ID, "login-button")
         loginBtn.click()
-        errorMessage = driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
-       #gereksinim dosyamızda nasıl olması gerektiği burada.
+        errorMessage = driver.find_element(By.XPATH, "//*[@id='login_button_container']/div/form/div[3]/h3")
         testResult = errorMessage.text == "Epic sadface: Username and password do not match any user in this service"
         print(f"BAŞARISIZ GİRİŞ TESTİ SONUCU: {testResult}")
-        
 
-
-    #-Kullanıcı adı ve şifre alanları boş geçildiğinde uyarı mesajı olarak "Epic sadface: Username is required" gösterilmelidir.
     def test_both(self):
-        driver = self.all_setting()
-        WebDriverWait(driver,5).until(EC.visibility_of_all_elements_located((By.ID,"user-name")))
-        userName=driver.find_element(By.ID,"user-name")
+        driver = self.local()
+        WebDriverWait(driver, 5).until(EC.visibility_of_all_elements_located((By.ID, "user-name")))
+        userName = driver.find_element(By.ID, "user-name")
         userName.send_keys("")
-        password=driver.find_element(By.ID,"password")
+        password = driver.find_element(By.ID, "password")
         password.send_keys("")
-        
-        loginButton= driver.find_element(By.ID,"login-button")
+        loginButton = driver.find_element(By.ID, "login-button")
         loginButton.click()
-        
-        errorMessage =driver.find_element(By.CSS_SELECTOR,"h3")
+        errorMessage = driver.find_element(By.CSS_SELECTOR, "h3")
         testResult = errorMessage.text == "Epic sadface: Username is required"
         print(f"KULLANICI ADI VE ŞİFRE ALANLARININ BOŞ GEÇİLDİĞİ TESTİ SONUCU: {testResult}")
 
-
-    #-Sadece şifre alanı boş geçildiğinde uyarı mesajı olarak "Epic sadface: Password is required" gösterilmelidir.
     def fpassword(self):
-        driver = self.all_setting()
-        WebDriverWait(driver,5).until(EC.visibility_of_all_elements_located((By.ID,"user-name")))
-        userName=driver.find_element(By.ID,"user-name")
+        driver = self.local()
+        WebDriverWait(driver, 5).until(EC.visibility_of_all_elements_located((By.ID, "user-name")))
+        userName = driver.find_element(By.ID, "user-name")
         userName.send_keys("standard_user")
-        password=driver.find_element(By.ID,"password")
+        password = driver.find_element(By.ID, "password")
         password.send_keys("")
-        
-        loginButton= driver.find_element(By.ID,"login-button")
+        loginButton = driver.find_element(By.ID, "login-button")
         loginButton.click()
-        
-        errorMessage =driver.find_element(By.XPATH,"//div[@id='login_button_container']//form//h3")
+        errorMessage = driver.find_element(By.XPATH, "//div[@id='login_button_container']//form//h3")
         testResult = errorMessage.text == "Epic sadface: Password is required"
         print(f"ŞİFRE ALANININ  BOŞ GEÇİLDİĞİ TESTİ SONUCU: {testResult}")
-    
-    #-Kullanıcı adı "locked_out_user" şifre alanı "secret_sauce" gönderildiğinde "Epic sadface: Sorry, this user has been locked out." mesajı gösterilmelidir.
+
     def locked_out(self):
-        driver = self.all_setting()
-        userName=driver.find_element(By.ID,"user-name")
+        driver = self.local()
+        userName = driver.find_element(By.ID, "user-name")
         userName.send_keys("locked_out_user")
-        
-        password=driver.find_element(By.ID,"password")
+        password = driver.find_element(By.ID, "password")
         password.send_keys("secret_sauce")
-        
-        loginButton= driver.find_element(By.ID,"login-button")
+        loginButton = driver.find_element(By.ID, "login-button")
         loginButton.click()
-        
-        errorMessage =driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
+        errorMessage = driver.find_element(By.XPATH, "//*[@id='login_button_container']/div/form/div[3]/h3")
         testResult = errorMessage.text == "Epic sadface: Sorry, this user has been locked out."
         print(f"ŞİFRE ALANININ  BOŞ GEÇİLDİĞİ TESTİ SONUCU: {testResult}")
-    #-Kullanıcı adı "standard_user" şifre "secret_sauce" gönderildiğinde kullanıcı "/inventory.html" sayfasına gönderilmelidir. Giriş yapıldıktan sonra kullanıcıya gösterilen ürün sayısı "6" adet olmalıdır.   
-    def login_list(self):
-        driver = self.all_setting()
-        userName=driver.find_element(By.ID,"user-name")
-        userName.send_keys("standard_user")
-        password=driver.find_element(By.ID,"password")
-        password.send_keys("secret_sauce")
-        loginButton= driver.find_element(By.ID,"login-button")
-        loginButton.click()
-        WebDriverWait(driver,5).until(EC.url_to_be("https://www.saucedemo.com/inventory.html"))
 
-        p_list = driver.find_elements(By.CLASS_NAME,"inventory_item")
+    def login_list(self):
+        driver = self.local()
+        userName = driver.find_element(By.ID, "user-name")
+        userName.send_keys("standard_user")
+        password = driver.find_element(By.ID, "password")
+        password.send_keys("secret_sauce")
+        loginButton = driver.find_element(By.ID, "login-button")
+        loginButton.click()
+        WebDriverWait(driver, 5).until(EC.url_to_be("https://www.saucedemo.com/inventory.html"))
+        p_list = driver.find_elements(By.CLASS_NAME, "inventory_item")
         testResult = len(p_list) == 6
         print(f"Görütülenen sayfada 6 adet ürün vardır.: {(testResult)}")
-        
 
-    #Başarılı giriş testi
     def t_login(self):
-        driver = self.all_setting()
-        WebDriverWait(driver,5).until(EC.visibility_of_all_elements_located((By.ID,"user-name")))
-        userNameInput = driver.find_element(By.ID,"user-name")
-        passwordInput = driver.find_element(By.ID,"password")
+        driver = self.local()
+        WebDriverWait(driver, 5).until(EC.visibility_of_all_elements_located((By.ID, "user-name")))
+        userNameInput = driver.find_element(By.ID, "user-name")
+        passwordInput = driver.find_element(By.ID, "password")
         sleep(1)
         userNameInput.send_keys("problem_user")
         passwordInput.send_keys("secret_sauce")
-        WebDriverWait(driver,5).until(EC.visibility_of_all_elements_located((By.ID,"login-button")))   
-        loginBtn = driver.find_element(By.ID,"login-button")
+        WebDriverWait(driver, 5).until(EC.visibility_of_all_elements_located((By.ID, "login-button")))
+        loginBtn = driver.find_element(By.ID, "login-button")
         loginBtn.click()
-        
-        expected_url = "https://www.saucedemo.com/inventory.html"
-        actual_url = driver.current_url
-        if actual_url == expected_url:
-            print("Giriş başarılı.")
-        else:
-            print("Giriş başarısız.")
-        
+        assert"https://www.saucedemo.com/inventory.html" in self.driver.current_url, "Giriş başarısız"
+        print("Giriş başarılı")
+       
 
 
-testClass = test_Saurce()
+testClass = TestSource()
 testClass.test_invalid_login()
 testClass.test_both()
 testClass.fpassword()
 testClass.login_list()
 testClass.t_login()
+
  
 
 
